ae4a946992fdc93abe911ae3f6ceeac51c29de6e
Rodrigo Raminelli <rraminelli@gmail.com>
2022-10-28 23:50:06 +0200

    aula 6

--- src/br/com/letscode/turmaitau/carros/Carro.java
+++ src/br/com/letscode/turmaitau/carros/Carro.java
@@ -1,16 +1,28 @@
 package br.com.letscode.turmaitau.carros;
 
+import java.util.Objects;
+
+/*
+    Toda classe herda é filha da classe PAI Object
+ */
+
 public class Carro {
 
+    //atributos
     String marca = null;
     String modelo;
     String placa;
     String cor;
+    //se uso a classe Integer (classe wrapper de in) a inicializacao é sempre NULL
     Integer anoFabricacao = null;
 
-    boolean ligado = false;
-    int velocidade = 0;
+    //se uso o primitivo a inicializacao é sempre "= false", é mesma coisa que "boolean ligado = false;"
+    boolean ligado;
 
+    //se uso o primitivo a inicializacao é sempre "= 0", é mesma coisa que "int velocidade = 0;"
+    int velocidade;
+
+    //metodo
     void ligar() {
         this.ligado = true;
     }
@@ -20,10 +32,15 @@ public class Carro {
     }
 
     void imprimirToString() {
-        System.out.println("this = " + this.toString());
-        System.out.println("super = " + super.toString());
+        System.out.println("this = " + this.toString()); //imprime o toString implementado nessa classe
+        System.out.println("super = " + super.toString()); //imprime o toString da classe PAI Object
+        System.out.println("hash this = " + this.hashCode()); //imprime o hashCode implementado nessa classe
+        System.out.println("hash super = " + super.hashCode()); //imprime o hashCode da classe PAI Object
     }
 
+    /*
+        toString => representacao do estado do objeto em String
+     */
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("Carro{");
@@ -37,4 +54,24 @@ public class Carro {
         sb.append('}');
         return sb.toString();
     }
+
+    /*
+        hashCode => representacao do estado do objeto em hash
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(marca, modelo, placa, cor, anoFabricacao);
+    }
+
+    /*
+       equals => faz a comparacao dos estados entre dois objetos
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Carro carro = (Carro) o;
+        return Objects.equals(marca, carro.marca) && Objects.equals(modelo, carro.modelo) && Objects.equals(placa, carro.placa) && Objects.equals(cor, carro.cor) && Objects.equals(anoFabricacao, carro.anoFabricacao);
+    }
+
 }
--- src/br/com/letscode/turmaitau/carros/CarroMain.java
+++ src/br/com/letscode/turmaitau/carros/CarroMain.java
@@ -6,26 +6,31 @@ public class CarroMain {
 
     public static void main(String[] args) {
 
+        //criacao do objeto carro audi
         Carro audi = new Carro();
         audi.modelo = "A1";
         audi.marca = "Audi";
         audi.cor = "Branco";
         audi.anoFabricacao = 2020;
         audi.placa = "XYZ12345";
+        //acelerando o carro audi
         audi.acelerar();
 
+        //criacao do objeto carro nissao
         Carro nissan = new Carro();
         nissan.modelo = "March";
         nissan.marca = "Nissan";
         nissan.cor = "Branco";
         nissan.anoFabricacao = 2020;
         nissan.placa = "ABC12345";
+        //acelerando o carro nissan
         nissan.acelerar();
         nissan.acelerar();
 
         System.out.println("audi = " + audi.toString());
         System.out.println("nissan = " + nissan);
 
+        //mudando o modelo, ou seja, mudando o estado do meu objeto
         audi.modelo = "A2";
         nissan.modelo = "Versa";
 
@@ -40,14 +45,17 @@ public class CarroMain {
         Carro golPreto = new Carro();
         golPreto.modelo = "Gol";
 
-        System.out.println( "modelo golBranco = golPreto? " + (golPreto.modelo.equals(golBranco.modelo) ));
+        System.out.println( "modelo golBranco = golPreto? " + (golPreto.equals(golBranco) ));
 
         Integer i1 = 1;
         Integer ii1 = new Integer("1");
 
+        //cuidado com a comparacao entre objetos
         System.out.println("i1 = " + i1);
         System.out.println("ii1 = " + ii1);
         System.out.println("== " + (ii1.intValue() == i1.intValue()) );
+        System.out.println("== " + (ii1 == i1) );
+        System.out.println("== " + ( ii1.equals(i1)) );
 
         audi.imprimirToString();
 
